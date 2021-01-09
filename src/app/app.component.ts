9396c71b1c3b837160ceb2a662d184996d7fe8ca
Muhammad Ehtesham <muhammadehteshambhatti@gmail.com>
2021-01-09 21:54:33 +0500

    auth0 implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +10,70 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   form!: FormGroup;
-  constructor(public http: HttpClient) {}
+
+  customers!: any[];
+  constructor(public http: HttpClient, public auth: AuthService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
+      id: new FormControl(''),
       name: new FormControl('', Validators.required),
       email: new FormControl(''),
+    });
+
+    this.http.get('http://localhost:3000/').subscribe((customersResponse) => {
+      this.customers = customersResponse as any;
     });
   }
 
   Save() {
     var obj = {
+      id: this.form.value.id,
       name: this.form.value.name,
       email: this.form.value.email,
     };
-    console.log(obj);
-    this.http.post('http://localhost:3000/', obj).subscribe((success) => {
-      console.log(success);
-      this.form.reset();
-      this.form.markAsUntouched();
-      this.form.markAsPristine();
+    console.log(this.form.value);
+    if (obj.id == '' || obj.id == null) {
+      this.http.post('http://localhost:3000/', obj).subscribe((success) => {
+        this.form.reset();
+        this.form.markAsUntouched();
+        this.form.markAsPristine();
+        this.customers.push(success);
+      });
+    } else {
+      this.http
+        .put('http://localhost:3000/' + obj.id, obj)
+        .subscribe((success) => {
+          this.form.reset();
+          this.form.markAsUntouched();
+          this.form.markAsPristine();
+
+          const currentObjectIndex = this.customers.findIndex((customer) => {
+            return customer.id === obj.id;
+          });
+          this.customers.splice(currentObjectIndex, 1, obj);
+        });
+    }
+  }
+
+  EditCustomer(id: any) {
+    const customer = this.customers.find((customer) => {
+      return customer.id === id;
+    });
+
+    this.form.patchValue({
+      id: customer.id,
+      name: customer.name,
+      email: customer.email,
+    });
+  }
+
+  DeleteCustomer(id: any) {
+    this.http.delete('http://localhost:3000/' + id).subscribe((success) => {
+      var newCustomers = this.customers.filter((customer) => {
+        return customer.id !== id;
+      });
+      this.customers = newCustomers;
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
+    MatIconModule,
     BrowserModule,
+    MatCardModule,
     MatInputModule,
     MatButtonModule,
     AppRoutingModule,
@@ -21,8 +28,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatFormFieldModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+
+    AuthModule.forRoot({
+      domain: 'dev-p4jles0i.us.auth0.com',
+      clientId: 'Rrm8OURwwuHjp90PtoHQyXWXzskC2GGN',
+      cacheLocation: 'localstorage',
+      useRefreshTokens: true,
+      audience: 'muhammadehteshambhatti',
+      httpInterceptor: {
+        allowedList: [`${'http://localhost:3000/'}/*`],
+      },
+    }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
